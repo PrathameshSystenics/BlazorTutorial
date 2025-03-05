bc89b7ceeb44f4809539c197539214adc5431d95
Prathamesh Dhande <prathamesh.d@systenics.com>
2025-03-05 09:00:10 +0530

    Blazor Basics Tutorial

---
+++ BlazorTutorial.Client/FilledTodos.cs
@@ -0,0 +1,29 @@
+﻿using BlazorTutorial.Client.Models;
+
+namespace BlazorTutorial.Client
+{
+    public class FilledTodos
+    {
+        public List<TodoItem> GetFilledTodoItems()
+        {
+            return new List<TodoItem>()
+            {
+                new TodoItem()
+                {
+                    IsDone=false,
+                    Title="First Todo"
+                },
+                new TodoItem()
+                {
+                    IsDone=false,
+                    Title="Second Todo"
+                },
+                new TodoItem()
+                {
+                    IsDone=false,
+                    Title="Third todo"
+                }
+            };
+        }
+    }
+}
--- BlazorTutorial.Client/Program.cs
+++ BlazorTutorial.Client/Program.cs
@@ -1,5 +1,6 @@
+using BlazorTutorial.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddScoped<FilledTodos>();
 await builder.Build().RunAsync();
